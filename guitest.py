71fad5f333035fa229a2b02537ec3219b0893a27
kbrown900 <74275228+kbrown900@users.noreply.github.com>
2025-02-05 15:49:40 -0800

    fix file formating

--- guitest.py
+++ guitest.py
@@ -11,6 +11,9 @@ def reset():
     txt.delete(0,END)
     lbl.configure(text = "Test Words for input")
 
+def closeProgram():
+    window.quit()
+
 #Menu bar
 menu_bar = Menu(window)
 
@@ -19,7 +22,7 @@ menu_1 = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label='File', menu=menu_1)
 menu_1.add_command(label='New', command=reset)
 menu_1.add_separator()
-menu_1.add_command(label='Exit',command=window.quit)
+menu_1.add_command(label='Exit',command=closeProgram)
 window.config(menu=menu_bar)
 
 #Window Content
@@ -30,22 +33,25 @@ lbl.grid()
 txt = Entry(window, width=10)
 txt.grid(column=1, row=0)
 
-#funciton for buttons
+#function for buttons
 def clicked():
     result = "You wrote: " + txt.get()
     lbl.configure(text = result)
 
-#Configure button
+#Configure buttons
 btn = Button(window, text = "Click Here",
              fg = "red", command=clicked)
 
-#Configure Reset
 btn_reset = Button(window, text = "Reset",
              fg = "Black", command=reset)
+
+btn_close = Button(window, text='Close',
+                   fg = "Black", command=closeProgram)
 
 #Set buttons to grid
 btn.grid (column=2, row=0)
 btn_reset.grid (column=3, row=0)
+btn_close.grid(column=6, row=1)
 
 #Execute window
 window.mainloop()
